54e69d8095058606868ad1b79c4eaa34f5a0038e
Aluno <aluno@etec.sp.gov.br>
2022-10-26 20:44:08 -0300

    Aula 5 - Repetições definidas

--- Aula 6/README.md
+++
@@ -1,2 +0,0 @@
-# pw1_script
-Projeto para JavaScript modular em sistema de Vue.Js (Vanilla) para aprendizagem de execução de micro web aplicações.
---
+++ Aula5/src/index.js
@@ -0,0 +1,7 @@
+function repeticao(indice, multiplo) {
+    for (let i = 0; i <= indice; i++) {
+        console.log(i * multiplo);
+    }
+}
+
+console.log(repeticao(30, 7));
