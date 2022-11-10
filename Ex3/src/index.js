2ca885e690f9138aee1d9366fb9e596d87330037
RafaelTabolka <rafaeltabolka@hotmail.com>
2022-11-10 02:31:00 -0300

    Desafio aula 3 feito!

--- Ex3/src/index.js
+++ Ex3/src/index.js
@@ -5,17 +5,35 @@ entre 1 e 60 aleatóriamente, com as seguintes condições:
 3º Os 3 primeiros números não devem estar na casa 0 ao 20 e do 21 ao 45
 4º Os 3 últimos devem estar na casa dos 46 ao 55 e o último 56 até 60. */
 
+let aleatorios = new Array();
 
-function sorteio() {
-    let aleatorios = []
+const fazNumeroAleatorio = () => {
+  for (let i = 0; i < 6; i++) {
+    let aleatorio = Math.floor(Math.random() * 60);
 
-    for (let i = 1; i <= 6; i++) {
-        let numAleatorio = Math.floor(Math.random() * 60)
-
-        aleatorios.push(numAleatorio[i])
-        
+    for (let i = 0; i < aleatorios.length; i++) {
+      while (aleatorio == aleatorios[i]) {
+        aleatorio = Math.floor(Math.random() * 60);
+        i = 0;
+      }
     }
 
-}
+    if (aleatorio > 45 && aleatorio <= 55 && i < 5) {
+      aleatorios.push(aleatorio);
+    } else if (aleatorio >= 56 && i == 5) {
+      aleatorios.push(aleatorio);
+    } else {
+      i--;
+    }
 
-console.log(sorteio()); 
+    /* if(i == 5 && aleatorios.length != 6){
+      while(aleatorios.length != 0){
+        aleatorios.shift()
+      }
+    } */
+  }
+
+  return aleatorios.sort();
+};
+
+console.log(fazNumeroAleatorio());
